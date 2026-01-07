1fc5f1550dbcb030f768f47d9d2534a77a05b337
nishadmahmud <mahmudnishad253@gmail.com>
2026-01-08 03:18:05 +0600

    fix: add missing useRouter import in cart page

--- app/cart/page.js
+++ app/cart/page.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useCart } from '@/context/CartContext';
 import { useAuth } from '@/context/AuthContext';
 
