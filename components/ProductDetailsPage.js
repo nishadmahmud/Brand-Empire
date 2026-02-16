a7faeaaa09b1042d24789539311f0307559c7e11
nishadmahmud <mahmudnishad253@gmail.com>
2026-02-16 20:35:39 +0600

    return

--- components/ProductDetailsPage.js
+++ components/ProductDetailsPage.js
@@ -645,6 +645,12 @@ const ProductDetailsPage = ({ productId }) => {
                                     </div>
                                 </div>
                             )}
+                            {product.return_delivery_days && (
+                                <div className="mt-4 flex items-center gap-2 text-sm text-gray-700">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-gray-500"><path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"></path><path d="M3 3v5h5"></path></svg>
+                                    <span>{product.return_delivery_days}</span>
+                                </div>
+                            )}
                         </div>
 
                         {/* Offers */}
